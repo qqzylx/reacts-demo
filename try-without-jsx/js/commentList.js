22a91df7b1e0afa4742cd3644372c55926ef8d32
Michael Zhang <zhangquanqiang@live.com>
2016-10-31 18:55:49 +0800

    comment list fix

--- try-without-jsx/js/commentList.js
+++ try-without-jsx/js/commentList.js
@@ -10,10 +10,9 @@ import Comment from './comment';
 
 var ComponentList = c({
     render: function () {
-        var comment3 = null;
+        var comments = [];
         var data = this.props.data;
         if (data && data.length > 0) {
-            var comments = [];
             data.forEach(function(element, index) {
                 const comment = e(Comment, {
                     author: element.author
