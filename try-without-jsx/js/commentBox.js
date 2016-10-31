5b357055e1fd5197eeb3cc1b14454090aaeb4aa6
Michael Zhang <zhangquanqiang@live.com>
2016-10-31 19:17:25 +0800

    simulate getting data from server

--- try-without-jsx/js/commentBox.js
+++ try-without-jsx/js/commentBox.js
@@ -8,17 +8,40 @@ import React, {
 import CommentForm from './commentForm';
 import CommentList from './commentList';
 
-var data = [
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+
+var simulateData = [
   {id: 1, author: "Pete Hunt", text: "This is one comment"},
   {id: 2, author: "Jordan Walke", text: "This is second comment"},
-  {id: 3, author: "Michael Zhang", text: "This is third comment"},
+  {id: 3, author: "Quanqiang Zhang", text: "This is third comment"},
 ];
 
 var CommentBox = c({
+    getInitialState: function () {
+        return {data: []};
+    },
+    componentDidMount: function () {
+        var self = this;
+        fetch(this.props.url, {
+            method: 'GET'
+        }).then(function(res) {
+            console.log(res.status);
+            console.log(res);
+
+            // don't care result from request, just use the simulateData
+            self.setState({data: simulateData});
+        }).catch(function(err) {
+            console.log('fetch error');
+
+            // don't care result from request, just use the simulateData
+            self.setState({data: simulateData});
+        });
+    },
     render: function () {
         const h1 = e('h1', {}, 'Comments');
         const commentList = e(CommentList, {
-            data: data
+            data: this.state.data
         });
         const commentForm = e(CommentForm);
         const div = e('div', {
--- try-without-jsx/js/index.js
+++ try-without-jsx/js/index.js
@@ -9,6 +9,9 @@ import ReactDom from 'react-dom';
 import CommentBox from './commentBox';
 
 ReactDom.render(
-    e(CommentBox),
+    e(CommentBox, {
+        // use the valid url to simulate getting success response from server
+        url: 'http://www.reactjs.cn/'
+    }),
     document.getElementById('content')
 );
